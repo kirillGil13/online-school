d0dd3f1110d3bebc7122b26f435463197faee9fa
SakuraBurst <colapes2@gmail.com>
2021-01-26 16:53:44 +0300

    work

--- src/repository/provider.ts
+++ src/repository/provider.ts
@@ -1,11 +1,13 @@
 import { IProvider } from './provider.types';
 import { CandidatesRepository } from '@/repository/candidates';
 import { PartnersRepository } from '@/repository/partners';
+import {UserUpdateRepository} from '@/repository/userUpdate';
 import {PresentationsRepository} from '@/repository/presentations';
 
 export const provider = (): IProvider => ({
     candidates: new CandidatesRepository(),
     partners: new PartnersRepository(),
+    userUpdate: new UserUpdateRepository(),
     presentations: new PresentationsRepository()
 });
 
--- src/repository/provider.types.ts
+++ src/repository/provider.types.ts
@@ -1,9 +1,11 @@
 import { ICandidatesRepository } from '@/repository/candidates';
 import { IPartnersRepository } from '@/repository/partners';
+import {IUserUpdateRepository} from '@/repository/userUpdate';
 import {IPresentationRepository} from '@/repository/presentations';
 
 export interface IProvider {
     candidates: ICandidatesRepository;
     partners: IPartnersRepository;
+    userUpdate: IUserUpdateRepository;
     presentations: IPresentationRepository;
 }
