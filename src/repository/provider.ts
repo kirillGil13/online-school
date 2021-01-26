d9b4491d64e91843ea0f039d05e75c7080134767
SakuraBurst <colapes2@gmail.com>
2021-01-26 19:04:18 +0300

    work

--- src/repository/provider.ts
+++ src/repository/provider.ts
@@ -3,11 +3,20 @@ import { CandidatesRepository } from '@/repository/candidates';
 import { PartnersRepository } from '@/repository/partners';
 import {UserUpdateRepository} from '@/repository/userUpdate';
 import {PresentationsRepository} from '@/repository/presentations';
+import {CoursesRepository} from '@/repository/courses';
+import {CourseItemRepository} from '@/repository/courseItem';
+import {LessonItemRepository} from '@/repository/lessonItem';
+import {QuestionsRepository} from '@/repository/questions';
 
 export const provider = (): IProvider => ({
     candidates: new CandidatesRepository(),
     partners: new PartnersRepository(),
     userUpdate: new UserUpdateRepository(),
-    presentations: new PresentationsRepository()
+    presentations: new PresentationsRepository(),
+    courses: new CoursesRepository(),
+    courseItem: new CourseItemRepository(),
+    lessonItem: new LessonItemRepository(),
+    questions: new QuestionsRepository(),
+
 });
 
--- src/repository/provider.types.ts
+++ src/repository/provider.types.ts
@@ -2,10 +2,19 @@ import { ICandidatesRepository } from '@/repository/candidates';
 import { IPartnersRepository } from '@/repository/partners';
 import {IUserUpdateRepository} from '@/repository/userUpdate';
 import {IPresentationRepository} from '@/repository/presentations';
+import {ICoursesRepository} from '@/repository/courses';
+import {ICourseItemRepository} from '@/repository/courseItem';
+import {ILessonItemRepository} from '@/repository/lessonItem';
+import {IQuestionsItemRepository} from '@/repository/questions';
 
 export interface IProvider {
     candidates: ICandidatesRepository;
     partners: IPartnersRepository;
     userUpdate: IUserUpdateRepository;
     presentations: IPresentationRepository;
+    courses: ICoursesRepository;
+    courseItem: ICourseItemRepository;
+    lessonItem: ILessonItemRepository;
+    questions: IQuestionsItemRepository;
+
 }
