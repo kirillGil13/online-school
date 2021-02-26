635bcf2b84c2bcae386e6059d45a11694ca5c57d
kirill <kirill.gil@startted.com>
2021-02-26 17:03:28 +0300

    candidates landing

--- src/UI/landing/Landing.vue
+++ src/UI/landing/Landing.vue
@@ -1,10 +1,13 @@
 <template>
 <v-col>
-  <div>
+  <v-row class="d-flex justify-center align-center align-self-center">
     <iframe id="ytplayer" type="text/html" width="830" height="425"
             ref="mainVideo"
             src="http://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
             frameborder="0"/>
+  </v-row>
+  <div class="d-flex justify-space-between flex-row flex-nowrap" v-if="infoPackageItemLoaded">
+    <InfoPackageItemVideoComponent v-for="(item, index) in infoPackageItem.videos" :key="index" :info-package-item-video="item"/>
   </div>
 </v-col>
 </template>
@@ -12,10 +15,25 @@
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
 import CourseComponent from '../components/course/CourseComponent.vue';
+import InfoPackageItemVideoComponent from '../components/infoPackageItemVideo/InfoPackageItemVideoComponent.vue';
+import {InfoPackagesStore} from '../../store/modules/InfoPackages';
+import {IInfoPackageItem} from '../../entity/infoPackageItem/infoPackageItem.types';
 @Component({
-  components: {CourseComponent}
+  components: {InfoPackageItemVideoComponent, CourseComponent}
 })
 export default class Landing extends Vue {
+
+  get infoPackageItem(): IInfoPackageItem {
+    return InfoPackagesStore.infoPackageItem!;
+  }
+
+  get infoPackageItemLoaded(): boolean {
+    return InfoPackagesStore.infoPackageItemLoaded;
+  }
+
+  async created(): Promise<void> {
+    await InfoPackagesStore.fetchData(this.$route.params.id);
+  }
 }
 </script>
 
