6704e3c08d135bdfea801eb92f5141f5e336b3e1
kirill <kirill.gil@startted.com>
2021-04-12 10:51:55 +0300

    confirm

--- src/router/router.ts
+++ src/router/router.ts
@@ -2,7 +2,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import {RouterNameEnum} from '@/router/router.types';
-import {confirmGuard, leaderGuard, mainGuard} from '@/guard';
+import {leaderGuard, mainGuard} from '@/guard';
 
 Vue.use(Router);
 Vue.prototype.$routeRules = RouterNameEnum;
