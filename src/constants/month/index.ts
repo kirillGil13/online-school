5bc7309b2849faec0b88e3a2224957e50789cb6e
kirill <kirill.gil@startted.com>
2021-05-03 18:08:44 +0300

    refactor

--- src/constants/month/index.ts
+++ src/constants/month/index.ts
@@ -3,7 +3,7 @@ export const MONTHS = [
     {id: 2, value: 'Февраля'},
     {id: 3, value: 'Марта'},
     {id: 4, value: 'Апреля'},
-    {id: 5, value: 'Майя'},
+    {id: 5, value: 'Мая'},
     {id: 6, value: 'Июня'},
     {id: 7, value: 'Июля'},
     {id: 8, value: 'Августа'},
