92a63149e5e689cba08b503c2e3e9ff7097427cf
Aidar Maksimov <aydar@startted.com>
2021-05-25 14:39:23 +0300

    ON-234: Fixed show icon when messages was read

--- src/UI/components/chat/Conversations.vue
+++ src/UI/components/chat/Conversations.vue
@@ -14,7 +14,7 @@
         <div class="conversation__header mb-1 d-flex justify-space-between align-center flex-row">
           <div class="name">{{elem.account.name + '.' + elem.account.lastName[0].toUpperCase()}}</div>
           <div class="status" >
-            <template v-if="elem.countUnread === 0">
+            <template v-if="isReaded(elem.countUnread) === true">
               <svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9.68978 0.739633C9.89577 0.945624 9.91161 1.26976 9.73731 1.49392L9.68978 1.54776L3.97549 7.26204C3.7695 7.46803 3.44537 7.48388 3.2212 7.30958L3.16737 7.26204L0.881654 4.97633C0.658498 4.75317 0.658498 4.39136 0.881654 4.1682C1.08765 3.96221 1.41178 3.94637 1.63594 4.12067L1.68978 4.1682L3.57143 6.04941L8.88165 0.739633C9.10481 0.516476 9.46662 0.516476 9.68978 0.739633ZM13.1183 0.739633C13.3243 0.945624 13.3402 1.26976 13.1659 1.49392L13.1183 1.54776L7.40406 7.26204C7.1809 7.4852 6.8191 7.4852 6.59594 7.26204C6.38995 7.05605 6.3741 6.73192 6.5484 6.50775L6.59594 6.45392L12.3102 0.739633C12.5334 0.516476 12.8952 0.516476 13.1183 0.739633Z" fill="#434DFF"/>
               </svg>
@@ -91,6 +91,14 @@ export default class Conversations extends Vue {
     return today === lastDate ? time : currentDate;
   }
 
+
+  isReaded(count: number): boolean {
+    if(count === 0  || (this.$routeRules.ChatMain ===  this.$route.name) && this.$route.params.id === this.chousenChatId?.toString()) {
+      return true
+    }
+
+    return false;
+  }
   
 
   randomColor(i: number): string {
